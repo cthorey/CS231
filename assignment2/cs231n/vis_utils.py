ee8099704a29389a41ab320ffd295290834d6f5b
cthorey <clement.thorey@gmail.com>
2016-02-04 17:36:21 +0100

    Lets run some convnet

--- assignment2/cs231n/vis_utils.py
+++ assignment2/cs231n/vis_utils.py
@@ -1,73 +1,103 @@
 from math import sqrt, ceil
 import numpy as np
+import seaborn as sns
+import matplotlib.pylab as plt
+
 
 def visualize_grid(Xs, ubound=255.0, padding=1):
-  """
-  Reshape a 4D tensor of image data to a grid for easy visualization.
+    """
+    Reshape a 4D tensor of image data to a grid for easy visualization.
 
-  Inputs:
-  - Xs: Data of shape (N, H, W, C)
-  - ubound: Output grid will have values scaled to the range [0, ubound]
-  - padding: The number of blank pixels between elements of the grid
-  """
-  (N, H, W, C) = Xs.shape
-  grid_size = int(ceil(sqrt(N)))
-  grid_height = H * grid_size + padding * (grid_size - 1)
-  grid_width = W * grid_size + padding * (grid_size - 1)
-  grid = np.zeros((grid_height, grid_width, C))
-  next_idx = 0
-  y0, y1 = 0, H
-  for y in xrange(grid_size):
-    x0, x1 = 0, W
-    for x in xrange(grid_size):
-      if next_idx < N:
-        img = Xs[next_idx]
-        low, high = np.min(img), np.max(img)
-        grid[y0:y1, x0:x1] = ubound * (img - low) / (high - low)
-        # grid[y0:y1, x0:x1] = Xs[next_idx]
-        next_idx += 1
-      x0 += W + padding
-      x1 += W + padding
-    y0 += H + padding
-    y1 += H + padding
-  # grid_max = np.max(grid)
-  # grid_min = np.min(grid)
-  # grid = ubound * (grid - grid_min) / (grid_max - grid_min)
-  return grid
+    Inputs:
+    - Xs: Data of shape (N, H, W, C)
+    - ubound: Output grid will have values scaled to the range [0, ubound]
+    - padding: The number of blank pixels between elements of the grid
+    """
+    (N, H, W, C) = Xs.shape
+    grid_size = int(ceil(sqrt(N)))
+    grid_height = H * grid_size + padding * (grid_size - 1)
+    grid_width = W * grid_size + padding * (grid_size - 1)
+    grid = np.zeros((grid_height, grid_width, C))
+    next_idx = 0
+    y0, y1 = 0, H
+    for y in xrange(grid_size):
+        x0, x1 = 0, W
+        for x in xrange(grid_size):
+            if next_idx < N:
+                img = Xs[next_idx]
+                low, high = np.min(img), np.max(img)
+                grid[y0:y1, x0:x1] = ubound * (img - low) / (high - low)
+                # grid[y0:y1, x0:x1] = Xs[next_idx]
+                next_idx += 1
+            x0 += W + padding
+            x1 += W + padding
+        y0 += H + padding
+        y1 += H + padding
+    # grid_max = np.max(grid)
+    # grid_min = np.min(grid)
+    # grid = ubound * (grid - grid_min) / (grid_max - grid_min)
+    return grid
+
 
 def vis_grid(Xs):
-  """ visualize a grid of images """
-  (N, H, W, C) = Xs.shape
-  A = int(ceil(sqrt(N)))
-  G = np.ones((A*H+A, A*W+A, C), Xs.dtype)
-  G *= np.min(Xs)
-  n = 0
-  for y in range(A):
-    for x in range(A):
-      if n < N:
-        G[y*H+y:(y+1)*H+y, x*W+x:(x+1)*W+x, :] = Xs[n,:,:,:]
-        n += 1
-  # normalize to [0,1]
-  maxg = G.max()
-  ming = G.min()
-  G = (G - ming)/(maxg-ming)
-  return G
-  
+    """ visualize a grid of images """
+    (N, H, W, C) = Xs.shape
+    A = int(ceil(sqrt(N)))
+    G = np.ones((A * H + A, A * W + A, C), Xs.dtype)
+    G *= np.min(Xs)
+    n = 0
+    for y in range(A):
+        for x in range(A):
+            if n < N:
+                G[y * H + y:(y + 1) * H + y, x * W + x:(x + 1)
+                  * W + x, :] = Xs[n, :, :, :]
+                n += 1
+    # normalize to [0,1]
+    maxg = G.max()
+    ming = G.min()
+    G = (G - ming) / (maxg - ming)
+    return G
+
+
 def vis_nn(rows):
-  """ visualize array of arrays of images """
-  N = len(rows)
-  D = len(rows[0])
-  H,W,C = rows[0][0].shape
-  Xs = rows[0][0]
-  G = np.ones((N*H+N, D*W+D, C), Xs.dtype)
-  for y in range(N):
-    for x in range(D):
-      G[y*H+y:(y+1)*H+y, x*W+x:(x+1)*W+x, :] = rows[y][x]
-  # normalize to [0,1]
-  maxg = G.max()
-  ming = G.min()
-  G = (G - ming)/(maxg-ming)
-  return G
+    """ visualize array of arrays of images """
+    N = len(rows)
+    D = len(rows[0])
+    H, W, C = rows[0][0].shape
+    Xs = rows[0][0]
+    G = np.ones((N * H + N, D * W + D, C), Xs.dtype)
+    for y in range(N):
+        for x in range(D):
+            G[y * H + y:(y + 1) * H + y, x * W + x:(x + 1)
+              * W + x, :] = rows[y][x]
+    # normalize to [0,1]
+    maxg = G.max()
+    ming = G.min()
+    G = (G - ming) / (maxg - ming)
+    return G
 
 
+def inspect_solvers(solvers):
+    ''' solvers is a dict with key:solver.
+    Return a plot with the different loss on of top of another '''
 
+    fig = plt.figure(figsize=(10, 30))
+    height = len(solvers) + 1
+    ax = plt.subplot(height, 1, 1)
+    axs = [plt.subplot(height, 1, i + 2) for i in range(len(solvers))]
+
+    i = 0
+    for key, solver in solvers.iteritems():
+        ax.plot(solver.loss_history, label=key)
+        ax.legend(fontsize=24)
+        ax.tick_params(labelsize=24)
+        ax.set_xlabel('epoch')
+        ax.set_ylabel('accuracy')
+
+        axs[i].plot(solver.train_acc_history)
+        axs[i].plot(solver.val_acc_history)
+        axs[i].legend(['train', 'val'], loc='upper left')
+        axs[i].set_xlabel('epoch')
+        axs[i].set_ylabel('accuracy')
+        axs[i].set_title(key, fontsize=24)
+        i += 1
---
+++ assignment2/experiments/ThreeLayerConvnet/ThreeLayerConvnet.py
@@ -0,0 +1,71 @@
+import os
+import json
+
+DIR_CS231n = '/Users/thorey/Documents/MLearning/CS231/assignment2/'
+import sys
+sys.path.append(DIR_CS231n)
+import numpy as np
+import matplotlib.pyplot as plt
+from cs231n.classifiers.cnn import ThreeLayerConvNet
+from cs231n.data_utils import get_CIFAR10_data
+from cs231n.gradient_check import eval_numerical_gradient_array, eval_numerical_gradient
+from cs231n.layers import *
+from cs231n.fast_layers import *
+from cs231n.solver import Solver
+
+if __name__ == "__main__":
+    conf_file = sys.argv[1]
+
+    with open(conf_file, 'r') as f:
+        conf = json.load(f)
+
+    # Parameter extraction
+    # Model instance
+
+    input_dim = conf.get('input_dim', (3, 32, 32))
+    num_filters = conf.get('num_filters', 32)
+    filter_size = conf.get('filter_size', 7)
+    hidden_dim = conf.get('hidden_dim', 100)
+    num_classes = conf.get('num_classes', 9)
+    weight_scale = conf.get('weight_scale', 1e-3)
+    reg = conf.get('reg', 0.0)
+    dtype = conf.get('dtype', np.float32)
+    use_batchnorm = conf.get('use_batchnorm', True)
+
+    # Solver instance
+    update_rule = conf.get('update_rule', 'adam')
+    optim_config = conf.get('optim_config', {'learning_rate': 1e-3}),
+    lr_decay = conf.get('lr_decay', 1.0),
+    batch_size = conf.get('batch_size', 100),
+    num_epochs = conf.get('num_epochs', 10),
+    print_every = conf.get('print_every', 10),
+    verbose = conf.get('verbose', True)
+
+    # Load the (preprocessed) CIFAR10 data.
+    data = get_CIFAR10_data(DIR_CS231n)
+    for k, v in data.iteritems():
+        print '%s: ' % k, v.shape
+
+    # Initialize the model instance
+    model = ThreeLayerConvNet(input_dim=input_dim,
+                              num_filters=num_filters,
+                              filter_size=filter_size,
+                              hidden_dim=hidden_dim,
+                              num_classes=num_classes,
+                              weight_scale=weight_scale,
+                              reg=reg,
+                              dtype=dtype,
+                              use_batchnorm=use_batchnorm)
+
+    # Run the training
+    solver = Solver(model=model,
+                    data=data,
+                    update_rule=update_rule,
+                    optim_config=optim_config,
+                    lr_decay=lr_decay,
+                    batch_size=batch_size,
+                    num_epochs=num_epochs,
+                    print_every=print_every,
+                    verbose=verbose)
+
+    solver.train()
