f80cda3ed11cca5c6f01013140b93b3f7d915a4a
cthorey <clement.thorey@gmail.com>
2016-02-05 18:24:29 +0000

    ALmorst new conv, implement backward todo

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -7,9 +7,9 @@ from cs231n.layer_utils import *
 
 class FirstConvNet(object):
     """
-    A three-layer convolutional network with the following architecture:
+    A L-layer convolutional network with the following architecture:
 
-    [conv-relu-pool2x2]x(L-1) - affine - relu - affine - softmax
+    [conv-relu-pool2x2]xL - [affine - relu]xM - affine - softmax
 
     The network operates on minibatches of data that have shape (N, C, H, W)
     consisting of N images, each with height H and width W and with C input
@@ -17,7 +17,7 @@ class FirstConvNet(object):
     """
 
     def __init__(self, input_dim=(3, 32, 32), num_filters=[16, 32, 64], filter_size=3,
-                 hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0,
+                 hidden_dims=[100, 100], num_classes=10, weight_scale=1e-3, reg=0.0,
                  dtype=np.float32, use_batchnorm=False):
         """
         Initialize a new network.
@@ -27,90 +27,76 @@ class FirstConvNet(object):
         - num_filters: List of size  Nbconv+1 with the number of filters
         to use in each convolutional layer
         - filter_size: Size of filters to use in the convolutional layer
-        - hidden_dim: Number of units to use in the fully-connected hidden layer
+        - hidden_dims: Number of units to use in the fully-connected hidden layer
         - num_classes: Number of scores to produce from the final affine layer.
         - weight_scale: Scalar giving standard deviation for random initialization
           of weights.
         - reg: Scalar giving L2 regularization strength
         - dtype: numpy datatype to use for computation.
         """
+
         self.use_batchnorm = use_batchnorm
         self.params = {}
         self.reg = reg
         self.dtype = dtype
         self.bn_params = {}
 
+        self.filter_size = filter_size
+        self.L = len(num_filters)  # Number of weights
+        self.M = len(hidden_dims)  # Number of conv/relu/pool blocks
+
         # Size of the input
-        C, H, W = input_dim
+        Cinput, Hinput, Winput = input_dim
         stride_conv = 1  # stride
 
-        Nbconv = len(num_filters)
-
-        # Size of the output of the first conv
-        for i, F in enumerate(xrange(Nbconv)):
-            F = num_filters[i]
+        # Initialize the weight for the conv layers
+        F = [Cinput] + num_filters
+        for i in xrange(self.L):
+            idx = i + 1
             W = weight_scale * \
-                np.random.randn(F, C, filter_height, filter_width)
-            b = np.zeros(F)
-            self.params.update({'W' + str(i + 1): W,
-                                'b' + str(i + 1): b})
+                np.random.randn(
+                    F[i + 1], F[i], self.filter_size, self.filter_size)
+            b = np.zeros(F[i + 1])
+            self.params.update({'W' + str(idx): W,
+                                'b' + str(idx): b})
 
-        # The output size is given by (N,F,Hp,Wp)
-        Hp, Wp = self.Size_Conv(stride_conv, filter_size, H, W, Nbconv)
+        # Initialize the weights for the affine-relu layers
+        # Size of the last activation
+        Hconv, Wconv = self.Size_Conv(
+            stride_conv, self.filter_size, Hinput, Winput, self.L)
+        dims = [Hconv * Wconv * F[-1]] + hidden_dims
+        for i in xrange(self.M):
+            idx = self.L + i + 1
+            W = weight_scale * \
+                np.random.randn(dims[i], dims[i + 1])
+            b = np.zeros(dims[i + 1])
+            self.params.update({'W' + str(idx): W,
+                                'b' + str(idx): b})
 
-        # Hidden Affine layer
-        # Size of the parameter (F*Hp*Wp,H1)
-        # Input: (N,F*Hp*Wp)
-        # Output: (N,Hh)
-
-        Hh = hidden_dim
-        W1 = weight_scale * np.random.randn(F * Hp * Wp, Hh)
-        b1 = np.zeros(Hh)
-
-        # Output affine layer
-        # Size of the parameter (Hh,Hc)
-        # Input: (N,Hh)
-        # Output: (N,Hc)
-
-        Hc = num_classes
-        W2 = weight_scale * np.random.randn(Hh, Hc)
-        b2 = np.zeros(Hc)
-
-        self.params.update({'W1': W1,
-                            'W2': W2,
-                            'b1': b1,
-                            'b2': b2})
-
-        # With batch normalization we need to keep track of running means and
-        # variances, so we need to pass a special bn_param object to each batch
-        # normalization layer. You should pass self.bn_params[0] to the forward pass
-        # of the first batch normalization layer, self.bn_params[1] to the forward
-        # pass of the second batch normalization layer, etc.
-
-        if self.use_batchnorm:
-            print 'We use batchnorm here'
-            bn_param1 = {'mode': 'train',
-                         'running_mean': np.zeros(F),
-                         'running_var': np.zeros(F)}
-            gamma1 = np.ones(F)
-            beta1 = np.zeros(F)
-
-            bn_param2 = {'mode': 'train',
-                         'running_mean': np.zeros(Hh),
-                         'running_var': np.zeros(Hh)}
-            gamma2 = np.ones(Hh)
-            beta2 = np.zeros(Hh)
-
-            self.bn_params.update({'bn_param1': bn_param1,
-                                   'bn_param2': bn_param2})
-
-            self.params.update({'beta1': beta1,
-                                'beta2': beta2,
-                                'gamma1': gamma1,
-                                'gamma2': gamma2})
+        # Scoring layer
+        W = weight_scale * np.random.rand(dims[-1], num_classes)
+        b = np.zeros(num_classes)
+        self.params.update({'W' + str(self.L + self.M + 1): W,
+                            'b' + str(self.L + self.M + 1): b})
 
         for k, v in self.params.iteritems():
             self.params[k] = v.astype(dtype)
+
+    def Size_Conv(self, stride_conv, filter_size, H, W, Nbconv):
+        P = (filter_size - 1) / 2  # padd
+        Hc = (H + 2 * P - filter_size) / stride_conv + 1
+        Wc = (W + 2 * P - filter_size) / stride_conv + 1
+        width_pool = 2
+        height_pool = 2
+        stride_pool = 2
+        Hp = (Hc - height_pool) / stride_pool + 1
+        Wp = (Wc - width_pool) / stride_pool + 1
+        if Nbconv == 1:
+            return Hp, Wp
+        else:
+            H = Hp
+            W = Wp
+            return self.Size_Conv(stride_conv, filter_size, H, W, Nbconv - 1)
 
     def loss(self, X, y=None):
         """
@@ -121,23 +107,10 @@ class FirstConvNet(object):
         X = X.astype(self.dtype)
         mode = 'test' if y is None else 'train'
 
-        if self.use_batchnorm:
-            for key, bn_param in self.bn_params.iteritems():
-                bn_param[mode] = mode
-
         N = X.shape[0]
 
-        W1, b1 = self.params['W1'], self.params['b1']
-        W2, b2 = self.params['W2'], self.params['b2']
-        W3, b3 = self.params['W3'], self.params['b3']
-        if self.use_batchnorm:
-            bn_param1, gamma1, beta1 = self.bn_params[
-                'bn_param1'], self.params['gamma1'], self.params['beta1']
-            bn_param2, gamma2, beta2 = self.bn_params[
-                'bn_param2'], self.params['gamma2'], self.params['beta2']
-
         # pass conv_param to the forward pass for the convolutional layer
-        filter_size = W1.shape[2]
+        filter_size = self.filter_size
         conv_param = {'stride': 1, 'pad': (filter_size - 1) / 2}
 
         # pass pool_param to the forward pass for the max-pooling layer
@@ -150,42 +123,46 @@ class FirstConvNet(object):
         # variable.                                                                #
         #######################################################################
 
-        # Forward into the conv layer
-        x = X
-        w = W1
-        b = b1
-        if self.use_batchnorm:
-            beta = beta1
-            gamma = gamma1
-            bn_param = bn_param1
-            conv_layer, cache_conv_layer = conv_norm_relu_pool_forward(
-                x, w, b, conv_param, pool_param, gamma, beta, bn_param)
-        else:
-            conv_layer, cache_conv_layer = conv_relu_pool_forward(
-                x, w, b, conv_param, pool_param)
+        blocks = {}
+        blocks['h0'] = X
+        # Forward into the conv blocks
+        for i in xrange(self.L):
+            idx = i + 1
+            w = self.params['W' + str(idx)]
+            b = self.params['b' + str(idx)]
+            h = blocks['h' + str(idx - 1)]
+            print i, w.shape, b.shape, h.shape
+            h, cache_h = conv_relu_pool_forward(
+                h, w, b, conv_param, pool_param)
+            print h.shape, conv_param['pad'], conv_param['stride']
+            blocks['h' + str(idx)] = h
+            blocks['cache_h' + str(idx)] = cache_h
 
-        N, F, Hp, Wp = conv_layer.shape  # output shape
+        # Forward into the linear blocks
+        for i in xrange(self.M):
+            idx = self.L + i + 1
+            h = blocks['h' + str(idx - 1)]
+            if i == 0:
+                h = h.reshape(N, np.product(h.shape[1:]))
+            w = self.params['W' + str(idx)]
+            b = self.params['b' + str(idx)]
+            h, cache_h = affine_relu_forward(h, w, b)
+            blocks['h' + str(idx)] = h
+            blocks['cache_h' + str(idx)] = cache_h
 
-        # Forward into the hidden layer
-        x = conv_layer.reshape((N, F * Hp * Wp))
-        w = W2
-        b = b2
-        if self.use_batchnorm:
-            gamma = gamma2
-            beta = beta2
-            bn_param = bn_param2
-            hidden_layer, cache_hidden_layer = affine_norm_relu_forward(
-                x, w, b, gamma, beta, bn_param)
-        else:
-            hidden_layer, cache_hidden_layer = affine_relu_forward(x, w, b)
-        N, Hh = hidden_layer.shape
+        # Fnally Forward into the score
+        idx = self.L + self.M + 1
+        w = self.params['W' + str(idx)]
+        b = self.params['b' + str(idx)]
+        h = blocks['h' + str(idx - 1)]
+        h, cache_h = affine_forward(h, w, b)
+        blocks['h' + str(idx)] = h
+        blocks['cache_h' + str(idx)] = cache_h
 
-        # Forward into the linear output layer
-        x = hidden_layer
-        w = W3
-        b = b3
-        scores, cache_scores = affine_forward(x, w, b)
+        scores = blocks['h' + str(idx)]
 
+        return blocks
+        sys.exit()
         if y is None:
             return scores
 
@@ -197,10 +174,12 @@ class FirstConvNet(object):
         # for self.params[k]. Don't forget to add L2 regularization!               #
         #######################################################################
 
+        # Computing of the loss
         data_loss, dscores = softmax_loss(scores, y)
-        reg_loss = 0.5 * self.reg * np.sum(W1**2)
-        reg_loss += 0.5 * self.reg * np.sum(W2**2)
-        reg_loss += 0.5 * self.reg * np.sum(W3**2)
+        reg_loss = 0
+        for w in [self.params[f] for f in self.params.keys() if f[0] == 'W']:
+            reg_loss += 0.5 * self.reg * np.sum(w * w)
+
         loss = data_loss + reg_loss
 
         # Backpropagation
@@ -246,22 +225,6 @@ class FirstConvNet(object):
         #######################################################################
 
         return loss, grads
-
-    def Size_Conv(self, stride_conv, filter_size, H, W, Nbconv):
-        P = (filter_size - 1) / 2  # padd
-        Hc = (H + 2 * P - filter_size) / stride_conv + 1
-        Wc = (W + 2 * P - filter_size) / stride_conv + 1
-        width_pool = 2
-        height_pool = 2
-        stride_pool = 2
-        Hp = (Hc - height_pool) / stride_pool + 1
-        Wp = (Wc - width_pool) / stride_pool + 1
-        if Nbconv == 1:
-            return Hp, Wp
-        else:
-            H = Hp
-            W = Wp
-            return Size_Conv(self, filter_size, H, W, Nbconv - 1)
 
 
 class ThreeLayerConvNet(object):
